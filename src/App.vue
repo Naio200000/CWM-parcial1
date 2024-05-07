1573fe6350df854f2436d75dfb6d89fedf079065
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-06 22:01:41 -0300

    feat: add vue plugin

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+    return {
+        name: 'App',
+
+    }
+</script>
+
+<template>
+    <nav>
+
+    </nav>
+    <main>
+        <h1>Hola mundo</h1>
+    </main>
+    <footer>
+        <p>Copyright &copy; Naio</p>
+    </footer>
+
+</template>
