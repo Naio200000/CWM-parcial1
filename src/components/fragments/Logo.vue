99391b22789c665917e83da2dcc9e83566ce95d0
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-18 20:16:57 -0300

    feat: add Logo Compoenet

---
+++ src/components/fragments/Logo.vue
@@ -0,0 +1,15 @@
+<script>
+    import MainLogo from '../labels/MainLogo.vue';
+    import MainP from '../labels/MainP.vue';
+    export default {
+        name: 'Logo',
+        components: {MainLogo, MainP},
+    }
+</script>
+
+<template>
+    <div class="flex gap-4">
+        <MainLogo />
+        <MainP />
+    </div>
+</template>
