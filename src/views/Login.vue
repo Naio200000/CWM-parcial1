ca4af30a19cf3ee0193678e36d03bbd3cbd20dca
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-18 20:44:41 -0300

    feat: add styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,15 @@
 <script>
+    import MainH1 from '../components/labels/MainH1.vue';
     export default {
         name: 'Login',
+        components: {MainH1}
     }
 </script>
 
 <template>
-
-    <H1>Iniciar Sesion</H1>
+    <header class="container mx-auto mt-8">
+        <div>
+            <MainH1>Iniciar Sesion</MainH1>
+        </div>
+    </header>
 </template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,15 @@
 <script>
+    import MainH1 from '../components/labels/MainH1.vue';
     export default {
         name: 'Register',
+        components: {MainH1}
     }
 </script>
 
 <template>
-
-    <H1>Registrate</H1>
-    
+    <header class="container mx-auto mt-8">
+        <div>
+            <MainH1>Registrarse</MainH1>
+        </div>
+    </header>
 </template>
