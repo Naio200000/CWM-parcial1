95f1fa5f451b8c96db46bed655ee64bb3ea3b098
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-18 21:54:32 -0300

    feat: add firebase templete

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsFUobq4669r59jH9pYw8LLG5P45OR4Yk",
+  authDomain: "dv-cwm-alsinet.firebaseapp.com",
+  projectId: "dv-cwm-alsinet",
+  storageBucket: "dv-cwm-alsinet.appspot.com",
+  messagingSenderId: "115401855389",
+  appId: "1:115401855389:web:8e2e4a938c4a7088301f74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
