abc8b88e3e06e86c2e8de3b8c06a309dae81dd04
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-18 21:57:40 -0300

    feat: add firestore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDsFUobq4669r59jH9pYw8LLG5P45OR4Yk",
   authDomain: "dv-cwm-alsinet.firebaseapp.com",
@@ -12,3 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
