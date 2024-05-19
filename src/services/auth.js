d535a2369ebff480154f248ca5cee0ba18ef07a2
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-19 20:53:02 -0300

    feat: documentations

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,17 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
 
+/**
+ * Registra un usuario en la base de datos
+ * 
+ * @param {string} email email del usuario
+ * @param {string} password password del usuario
+ * @returns {Promise<null>}
+ */
 export function register(email, password){
     return createUserWithEmailAndPassword(auth, email, password)
             .then(userCredentials => {
-                console.log('OK')
+                console.log(userCredentials)
             })
             .catch(error => {
                 console.error(error.code)
