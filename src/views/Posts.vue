84b27c6034afb67e143ba530f13675dfc9f52559
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-18 23:17:22 -0300

    feat: add posting functionality

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,28 +1,72 @@
 <script>
+
+
+    const defaultpost = [
+        {
+            user: 'Naio',
+            post: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, perferendis? Nam vitae earum laboriosam magnam aspernatur ipsam placeat doloribus nesciunt.'
+        },
+        {
+            user: 'Megameg',
+            post: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, perferendis? Nam vitae earum laboriosam magnam aspernatur ipsam placeat doloribus nesciunt.'
+        },
+        {
+            user: 'Pexdromeg',
+            post: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, perferendis? Nam vitae earum laboriosam magnam aspernatur ipsam placeat doloribus nesciunt.'
+        },
+    ];
+
+
     import MainH1 from '../components/labels/MainH1.vue';
     export default {
         name: 'Register',
-        components: {MainH1}
+        components: {MainH1},
+        data (){
+            return {
+                newPost: {
+                    user: '',
+                    post: '',
+                },                
+                posts:[],
+            }
+        },
+        methods: {
+            submitPost() {
+                this.posts.push({
+                    user: this.newPost.user,
+                    post: this.newPost.post,
+                });
+            this.newPost.post = '';
+            },
+        },
+        mounted() {
+            this.posts = defaultpost;
+        },
     }
+
 </script>
 
 <template>
     <section class="container mx-auto mt-8">
+
         <div>
             <MainH1>Posts</MainH1>
         </div>
         <div>
             <h2 class="text-center text-2xl mb-6">Ingresa un Post</h2>
-            <form action="#">
+            <form 
+                action="#"
+                @submit.prevent="submitPost">
                 <div class="mb-4">
                     <label 
                         class="text-lg font-bold block"
                         for="usuario"
                         >Usuario:</label>
                     <input
-                        class="border border-black border-2 rounded p-2 m-1"
+                        class="border-black border-2 rounded p-2 m-1"
                         type="text" 
-                        id="usuario">
+                        id="usuario"
+                        v-model="newPost.user">
                 </div>
                 <div class="mb-4">
                     <label 
@@ -30,10 +74,10 @@
                         for="post"
                         >Publicacion</label>
                     <textarea 
-                        class="border border-black border-2 rounded p-2 m-1"
+                        class="border-black border-2 rounded p-2 m-1"
                         name="post" 
                         id="post"
-                        ></textarea>
+                        v-model="newPost.post"></textarea>
                 </div>
                 <button 
                     type="submit"
@@ -43,7 +87,14 @@
         </div>
         <div class="w-full border min-h-[500px] p-4">
             <h2 class="text-center text-2xl font-bold uppercase">Posts</h2>
-            <div class="m-2 p-4"></div>
+            <div class="m-2 p-4">
+                <ul>
+                    <li v-for="post in posts">
+                       <p><b>{{ post.user }}</b></p> 
+                       <p>{{ post.post }}</p> 
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 </template>
