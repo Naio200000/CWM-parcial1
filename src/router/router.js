f63923481b57f1a778ddbdfda28ef291e6d60faf
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-05-18 21:18:43 -0300

    reformat: ffor better reading

--- src/router/router.js
+++ src/router/router.js
@@ -4,18 +4,9 @@ import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 
 const routes = [
-    {
-        path: '/',
-        component: Home,
-    },
-    {
-        path: '/iniciar',
-        component: Login,
-    },
-    {
-        path: '/registrarse',
-        component: Register,
-    },
+    {path: '/',             component: Home,},
+    {path: '/iniciar',      component: Login,},
+    {path: '/registrarse',  component: Register,},
 ];
 
 const router = createRouter({
