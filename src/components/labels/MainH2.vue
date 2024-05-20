de1f8a07c40bebd6d4c3fbbef04c745fc4325ca8
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-19 23:00:28 -0300

    feat: add mainh2 comonent

---
+++ src/components/labels/MainH2.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        name: 'MainH2',
+    }
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold text-center text-black mb-4 p-2"><slot /></h1>
+</template>
