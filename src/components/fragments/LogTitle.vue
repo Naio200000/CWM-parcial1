cc220dc6b175faa6a80a81c3d3c7a09b6692073e
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-19 22:45:43 -0300

    feat: add logTitle component

---
+++ src/components/fragments/LogTitle.vue
@@ -0,0 +1,16 @@
+<script>
+    import MainH1 from '../labels/MainH1.vue';
+    import MainP from '../labels/MainP.vue';
+    export default {
+        name: 'LogTitle',
+        components: {MainH1, MainP},
+    }
+</script>
+
+<template>
+    <div class="my-8 text-center">
+        <MainH1>Gaming Room</MainH1>
+        <MainP class="text-xl">La primera Red Social para Gamers.</MainP>
+        <MainP class="text-xl">Conicate, Comparte y Relacionate con Gamers de Todos el mundo de una forma facil y amigable</MainP>
+    </div>
+</template>
