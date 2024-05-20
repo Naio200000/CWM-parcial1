86d86f616a4c98e4659fff1b8d9485a595f4daa8
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2024-05-20 12:49:36 -0300

    refactor: add MainImg component

--- src/components/fragments/ImageLogo.vue
+++ src/components/fragments/ImageLogo.vue
@@ -1,13 +1,14 @@
 <script>
 import { RouterLink } from 'vue-router';
-
+import MainImg from '../labels/MainImg.vue';
     export default {
         name: 'ImageLogo',
+        components: {MainImg},
     }
 </script>
 
 <template>
     <RouterLink to="/">
-        <img src="../../../img/icons/logo.png" alt="Logo">
+        <MainImg :src="'../../../img/icons/logo.png'" :alt="'Logo'"/>
     </RouterLink>
 </template>
