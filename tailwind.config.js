8a5a12a279a7092be457ca0075186deffb5fdb2a
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-19 22:25:12 -0300

    refactor: change color palet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,6 @@ export default {
     './src/**/*.{vue,js,jsx,ts,tsx}'
   ],
   theme: {
-    colors: {
-      'black': '#010C13',
-      'blue-dark': '#4C6A94',
-      'blue-light': '#4763FF',
-      'gray-dark': '#BFBFBF',
-      'gray-light': '#E8E8E8',
-    },
     extend: {
       gridTemplateRows: {
         'layout' : '70px 1fr 100px',
