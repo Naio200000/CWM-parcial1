ca7de0919538c4bfcf38e35edcc97a05911f203c
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-20 19:01:49 -0300

    feat: add mainrouterlink component

---
+++ src/components/labels/MainRouterLink.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        name: 'MainRouterLink',
+        props: ['to'],
+    }
+
+</script>
+
+<template>
+    <routerLink :to="to"><slot /></routerLink>
+</template>
