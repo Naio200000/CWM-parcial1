21938355913fa674cb4191de11b025540cd836c1
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2024-05-20 10:00:37 -0300

    erease coments

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -72,29 +72,3 @@
         </section>
     </div>
 </template>
-<!-- <template>
-    <header class="container mx-auto mt-8">
-        <div>
-            <MainH1>Registrarse</MainH1>
-        </div>
-
-        <div>
-            <form action="" @submit.prevent="submitRegister">
-                <div class="mb-4">
-                    <label class="text-lg font-bold block" for="username">Nombre de Usuario</label>
-                    <input v-model="user.username" class="border-black border-2 rounded p-2 m-1" type="text" id="username">
-                </div>
-                <div class="mb-4">
-                    <label class="text-lg font-bold block" for="email">Direccion de Mail</label>
-                    <input v-model="user.email" class="border-black border-2 rounded p-2 m-1" type="email" id="email">
-                </div>
-                <div class="mb-4">
-                    <label class="text-lg font-bold block" for="password">Contrasena</label>
-                    <input v-model="user.password" class="border-black border-2 rounded p-2 m-1" type="password" id="password">
-                </div>
-                <button 
-                    type="submit" class="mb-4 py-2 px-6 bg-blue-light text-gray-light font-bold rounded hover:bg-blue-dark transition-all">Registrarse</button>
-            </form>
-        </div>
-    </header>
-</template> -->
