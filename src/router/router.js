4d64c3f1af02a4b03bfa6e79b4bd141428626a81
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-19 22:10:09 -0300

    feat: add profile view

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import Profile from '../views/Profile.vue';
 import Posts from '../views/Posts.vue';
 
 const routes = [
     {path: '/',             component: Home,},
     {path: '/iniciar',      component: Login,},
     {path: '/registrarse',  component: Register,},
+    {path: '/perfil',       component: Profile,},
     {path: '/posts',        component: Posts,},
 ];
 
---
+++ src/views/Profile.vue
@@ -0,0 +1,16 @@
+<script>
+import MainH1 from '../components/labels/MainH1.vue';
+import MainP from '../components/labels/MainP.vue';
+    export default {
+        name: 'Home',
+        components: {MainH1, MainP},
+    }
+</script>
+
+<template>
+    <header class="container mx-auto mt-8">
+        <div>
+            <MainH1>Mi perfil</MainH1>
+        </div>
+    </header>
+</template>
