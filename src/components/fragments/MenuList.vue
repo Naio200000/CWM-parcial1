541da819ab4c3a7a9458c105a180983222752ca2
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-20 19:02:42 -0300

    feat: add menu list template

---
+++ src/components/fragments/MenuList.vue
@@ -0,0 +1,34 @@
+<script>
+import MainImg from '../labels/MainImg.vue';
+import MainRouterLink from '../labels/MainRouterLink.vue';
+
+    export default {
+        name: 'MenuList',
+        components: {MainRouterLink, MainImg},
+    }
+</script>
+
+<template>
+
+    <div class="h-full px-8 m-8 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
+        <ul class="py-8">
+            <li class="py-4 ps-2 text-xl font-bold">
+                <MainImg class="w-10 inline me-2" :alt="'megameg'" :src="'../img/icons/home.png'"/>
+                <MainRouterLink class="align-middle" :to="'/home'">Nombre de usuario</MainRouterLink>
+            </li>
+            <li class="py-4 ps-2 text-xl font-bold">
+                <MainImg class="w-10 inline me-2" :alt="'megameg'" :src="'../img/icons/home.png'"/>
+                <MainRouterLink class="align-middle" :to="'/home'">Usuario</MainRouterLink>
+            </li>
+            <li class="py-4 ps-2 text-xl font-bold">
+                <MainImg class="w-10 inline me-2" :alt="'megameg'" :src="'../img/icons/home.png'"/>
+                <MainRouterLink class="align-middle" :to="'/home'">Usuario</MainRouterLink>
+            </li>
+            <li class="py-4 ps-2 text-xl font-bold">
+                <MainImg class="w-10 inline me-2" :alt="'megameg'" :src="'../img/icons/home.png'"/>
+                <MainRouterLink class="align-middle" :to="'/home'">Usuario</MainRouterLink>
+            </li>
+        </ul>
+    </div>
+
+</template>
