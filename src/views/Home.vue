bd88c15023da3ebd8430ef5154046f02741d2c57
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-20 19:02:05 -0300

    refacto: sizing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,21 @@ import MainP from '../components/labels/MainP.vue';
 import MainNav from '../components/labels/MainNav.vue';
 import PostsList from '../components/fragments/PostsList.vue';
 import PostForm from '../components/fragments/PostForm.vue';
+import MenuList from '../components/fragments/MenuList.vue';
     export default {
         name: 'Home',
-        components: { MainH1, MainP, MainNav, PostsList, PostForm },
+        components: { MainH1, MainP, MainNav, PostsList, PostForm, MenuList },
     }
 </script>
 
 <template>
     <MainNav />
-    <section>
-        <aside>
-            <nav>
-                
-            </nav>
+    <section class="flex">
+        <aside class="w-1/4 hidden lg:block">
+            <MenuList />
         </aside>
-        <section>
-            <header class="container mx-auto mt-8">
+        <section class="w-1/2">
+            <header class="mt-8">
                 <div>
                     <MainH1 class="sr-only">Gaming Room</MainH1>
                 </div>
@@ -29,5 +28,8 @@ import PostForm from '../components/fragments/PostForm.vue';
                 <PostsList />
             </article>
         </section>
+        <article class="w-1/4 hidden lg:block">
+
+        </article>
     </section>
 </template>
