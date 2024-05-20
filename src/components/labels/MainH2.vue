7dd2951cbf4bb4f98cdcfcd5b8f5e43470c4b3da
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2024-05-20 09:59:54 -0300

    reformat: label

--- src/components/labels/MainH2.vue
+++ src/components/labels/MainH2.vue
@@ -5,5 +5,5 @@
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold text-center text-black mb-4 p-2"><slot /></h1>
+    <h2 class="text-3xl font-bold text-center text-black mb-4 p-2"><slot /></h2>
 </template>
