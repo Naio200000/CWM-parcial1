d1356c30e0702dfce61f8cfe541776f585dc9c2c
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-20 22:30:37 -0300

    feat: add getUserProfilebyId method

---
+++ src/services/userProfile.js
@@ -0,0 +1,19 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
+
+/**
+ * Trae los datos de un usuario segun si ID
+ * 
+ * @param {string} id 
+ */
+export async function getUserProfilebyId(id) {
+
+    const refUser = doc(db, `users/${id}`);
+
+    const docUser = await getDoc(refuser);
+
+    return {
+        id: docUser.id,
+        email: docUser.data().email,
+    }
+}
