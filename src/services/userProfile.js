9a83f9c1fd4cfc84560a09e87def81073e0b7674
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-24 19:57:17 -0300

    feat: add createUserProfile Method

--- src/services/userProfile.js
+++ src/services/userProfile.js
@@ -1,4 +1,4 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 /**
@@ -16,4 +16,18 @@ export async function getUserProfilebyId(id) {
         id: docUser.id,
         email: docUser.data().email,
     }
+}
+
+/**
+ * Registra los datos de un nuevo usuario en la bd
+ * 
+ * @param {string} id Id del usuario para ser el nombre del documento el la base de datos
+ * @param {{datos del usuario}} data datos del usuario para ser guardados
+ * @return {Promise}
+ */
+export async function createUserProfile(id, data) {
+
+    const refUser = doc(db, `user/${id}`);
+
+    await setDoc(refUser, data);
 }
