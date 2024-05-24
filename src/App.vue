01694a17337c29aabf28066daa4ea90e1a327e5c
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-24 19:44:28 -0300

    update visibility

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,33 @@ import Home from './views/Home.vue';
 import LogTitle from './components/fragments/LogTitle.vue';
 import MainFooter from './components/fragments/MainFooter.vue';
 import MainNav from './components/fragments/MainNav.vue';
+import { subscribeToAuth } from './services/auth';
+import MenuList from './components/fragments/MenuList.vue';
 export default {
     name: 'App',
-    components: {Home, LogTitle, MainFooter, MainNav},
+    components: {Home, LogTitle, MainFooter, MainNav, MenuList},
+    data() {
+            return {
+                authUser: {
+                    id: null,
+                    email: null,
+                },
+                unsubscribeToAUth: () => {},
+            };
+        },
     methods: {
         showNav(){
             if (this.$route.path == '/' || this.$route.path == '/registrarse') {
                 return false;
             }
             return true;
-        }
+        },
+    },
+    mounted() { 
+        this.unsubscribeToAUth = subscribeToAuth(userData => this.authUser = userData);
+    },
+    unmounted() {
+        this.unsubscribeToAUth();
     }
 }
 </script>
@@ -22,8 +39,15 @@ export default {
         <div class="bg-gray-300">
             <MainNav v-if="showNav()" />
         </div>
-        <main class="pb-8 bg-gray-300">
-            <routerView />
+        <main class="pb-8 bg-gray-300 flex">
+            <aside v-if="showNav()" class="w-1/4 hidden lg:block">
+                <MenuList />
+            </aside>
+            <section class="w-1/2 flex-grow">
+                <routerView />
+            </section>
+            <article v-if="showNav()" class="w-1/4 hidden lg:block">
+            </article>
         </main>
         <MainFooter />
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,31 +4,22 @@ import MainP from '../components/labels/MainP.vue';
 import MainNav from '../components/fragments/MainNav.vue';
 import PostsList from '../components/fragments/PostsList.vue';
 import PostForm from '../components/fragments/PostForm.vue';
-import MenuList from '../components/fragments/MenuList.vue';
     export default {
         name: 'Home',
-        components: { MainH1, MainP, MainNav, PostsList, PostForm, MenuList },
+        components: { MainH1, MainP, MainNav, PostsList, PostForm,  },
     }
 </script>
 
 <template>
-    <section class="flex">
-        <aside class="w-1/4 hidden lg:block">
-            <MenuList />
-        </aside>
-        <section class="w-1/2">
-            <header class="mt-8">
-                <div>
-                    <MainH1 class="sr-only">Gaming Room</MainH1>
-                </div>
-            </header>
-            <article class="overflow-y-scroll max-h-screen">
-                <PostForm />
-                <PostsList/>
-            </article>
-        </section>
-        <article class="w-1/4 hidden lg:block">
-
+    <section class="w-full">
+        <header class="mt-8">
+            <div>
+                <MainH1 class="sr-only">Gaming Room</MainH1>
+            </div>
+        </header>
+        <article class="overflow-y-scroll max-h-screen">
+            <PostForm />
+            <PostsList/>
         </article>
     </section>
 </template>
