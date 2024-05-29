a149d29da1c3ec02fa80d34979d27c6579d9c920
Naio200000 <nicolas.alsinet@gmail.com>
2024-05-29 00:29:16 -0300

    feat: add link to profile page

--- src/components/fragments/GamersList.vue
+++ src/components/fragments/GamersList.vue
@@ -1,9 +1,10 @@
 <script>
 import { subscribeToUsers } from '../../services/userProfile';
 import MainP from '../labels/MainP.vue';
+import MainRouterLink from '../labels/MainRouterLink.vue';
 export default {
     name: 'GamersList',
-    components: {MainP},
+    components: {MainP, MainRouterLink},
     data() {
         return {
             gamers: [],
@@ -23,11 +24,13 @@ export default {
 
 <template>
     <template v-for="gamer in gamers" :key="gamer.id">
-        <div class="w-10/12 max-w-post mx-auto my-4 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
-            <div class="pt-2 px-2">
+        <div class="w-10/12 flex items-end max-w-post mx-auto my-4 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
+            <div class="w-10/12 pt-2 px-2">
                 <MainP class="text-base py-2"><span class="font-bold">Nombre de Usuario: </span>{{ gamer.displayName }}</MainP>
                 <MainP class="text-base py-2"><span class="font-bold">Email: </span>{{ gamer.email }}</MainP>
-                
+            </div>
+            <div class="py-2">
+                <MainRouterLink :to="`/gamers/${gamer.id}`" href="#">Ver Perfil</MainRouterLink>
             </div>
         </div>
     </template>
