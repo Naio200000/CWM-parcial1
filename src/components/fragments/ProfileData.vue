1a7f136dcd3f557a04a98a9ac2e0f38101d01b17
Naio200000 <nicolas.alsinet@gmail.com>
2024-06-20 20:35:46 -0300

    feat: add bring post from each user

--- src/components/fragments/ProfileData.vue
+++ src/components/fragments/ProfileData.vue
@@ -2,11 +2,12 @@
 import { getUserProfilebyId } from '../../services/userProfile';
 import MainH2 from '../labels/MainH2.vue';
 import MainP from '../labels/MainP.vue';
+import UserPosts from './UserPosts.vue';
 
 export default {
 
     name: 'ProfileData.vue',
-    components: { MainP, MainH2 },
+    components: { MainP, MainH2, UserPosts },
     data() {
         return {
            authUser: {
@@ -22,7 +23,7 @@ export default {
     },
     async mounted() {
 
-        this.userData = await getUserProfilebyId(this.$route.params.id);
+        this.userData = await getUserProfilebyId(this.$route.params.id  );
 
     },
     unmounted() {
@@ -32,15 +33,18 @@ export default {
 </script>
 
 <template>
-    <div class="w-10/12 max-w-post mx-auto my-4 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
+    <section class="w-10/12 max-w-post mx-auto my-4 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
         <div class="pt-2 px-2">
             <MainH2>{{ userData.displayName }}</MainH2>
         </div>
         <div class="pt-2 px-2">
-            <MainP class="text-base pb-0"><span class="font-bold">Email: </span>{{userData.email}}</MainP>
+            <MainP class="text-base pb-0"><span class="font-bold">Email: </span>{{userData.email }}</MainP>
         </div>
         <div class="px-2 py-4">
             <MainP><span class="font-bold">Id: </span>{{ userData.id }}</MainP>
         </div>
-    </div>
+    </section>
+    <section class="py-4">
+        <UserPosts :userId="userData.id"/>
+    </section>
 </template>
