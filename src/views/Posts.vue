a82bed10a2acc28ae1c79c400939a12d1cc678d9
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2024-06-27 10:56:39 -0300

    refactor: composition api

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,34 +1,3 @@
-<!-- <script>
-import MainNav from '../components/fragments/MainNav.vue';
-import MenuList from '../components/fragments/MenuList.vue';
-import UserPosts from '../components/fragments/UserPosts.vue';
-import MainH1 from '../components/labels/MainH1.vue';
-import MainP from '../components/labels/MainP.vue';
-import { subscribeToAuth } from '../services/auth';
-    export default {
-        name: 'Posts',
-        components: { MainH1, MainP, MainNav, MenuList, UserPosts, },
-        data() {
-            
-            return {
-                authUser: {
-                    id: null,
-                    email: null
-                },
-                unsubscribeToAUth: () => {},
-            };
-        },
-        methods: {
-
-        },
-        mounted() {
-            this.unsubscribeToAUth = subscribeToAuth(userData => this.authUser = userData);
-        },
-        unmounted() {
-            this.unsubscribeToAUth();
-        },
-    }
-</script> -->
 <script setup>
 import UserPosts from '../components/fragments/UserPosts.vue';
 import MainH1 from '../components/labels/MainH1.vue';
