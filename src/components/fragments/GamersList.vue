2bed6d10ffa66834a269d764da489cd08c0cb2a3
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2024-06-27 13:37:04 -0300

    feat : add filkter for auth user

--- src/components/fragments/GamersList.vue
+++ src/components/fragments/GamersList.vue
@@ -3,6 +3,10 @@ import MainP from '../labels/MainP.vue';
 import MainRouterLink from '../labels/MainRouterLink.vue';
 import { subscribeToUsers } from '../../services/userProfile';
 import { onMounted, onUnmounted, ref } from 'vue';
+import { useAuth } from '../../composition/useAuth';
+
+const {user: authUser} = useAuth();
+
 const { gamers } = useGamerList();
 
 function useGamerList() {
@@ -18,7 +22,6 @@ function useGamerList() {
     onUnmounted(()=>{
         unsubscribeToUsers();
     })
-    console.log(gamers.value)
     return {
         gamers,
     }
@@ -26,7 +29,7 @@ function useGamerList() {
 </script>
 <template>
     <template v-for="gamer in gamers" :key="gamer.id">
-        <div class="w-10/12 flex items-end max-w-post mx-auto my-4 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
+        <div v-if="gamer.id != authUser.id" class="w-10/12 flex items-end max-w-post mx-auto my-4 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
             <div class="w-10/12 pt-2 px-2">
                 <MainP class="text-base py-2"><span class="font-bold">Nombre de Usuario: </span>{{ gamer.displayName }}</MainP>
                 <MainP class="text-base py-2"><span class="font-bold">Email: </span>{{ gamer.email }}</MainP>
