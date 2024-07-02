28e07e5373d94c24d302f93584f5f35eeeb267e0
Naio200000 <nicolas.alsinet@gmail.com>
2024-07-02 20:51:06 -0300

    feat: add edit post skeleton

--- src/components/fragments/posts/PostEditForm.vue
+++ src/components/fragments/posts/PostEditForm.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { updatePost, getPostById } from '../../../services/post';
+import PostingLoader from '../skeletons/PostingLoader.vue';
+import PostEditSkeleton from '../skeletons/PostEditSkeleton.vue';
 import MainH2 from '../../labels/MainH2.vue';
 import MainImg from '../../labels/MainImg.vue';
 import MainLabel from '../../labels/MainLabel.vue';
 import MainP from '../../labels/MainP.vue';
-import PostingLoader from '../skeletons/PostingLoader.vue';
+import { updatePost, getPostById } from '../../../services/post';
 import { useAuth } from '../../../composition/useAuth';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router'
@@ -16,6 +17,8 @@ const route = useRoute();
 const { post } = userGetPostById(route.params.id)
 
 const postinSkeleton = ref(false);
+
+const loadingSkeleton = ref(true);
 
 const feedbackMsg = ref('');
 
@@ -67,60 +70,65 @@ function selectPhoto(e) {
     <section>
         <section class="w-10/12 max-w-post mx-auto mb-8 rounded-lg shadow-lg shadow-slate-400 bg-gray-100">
             <MainH2 class="sr-only">Ingresar un Post</MainH2>
-            <form 
-                action="#"
-                @submit.prevent="submitPost">
-                <div class="">
-                    <MainLabel for="usuario">Usuario :</MainLabel>
-                    <input
-                        class="px-4 py-2 text-xl font-bold rounded-lg"
-                        type="text" 
-                        id="usuario"
-                        disabled
-                        v-model="authUser.displayName">
-                </div>
-                <template v-if="post.photoURL">
-                    <div class="w-6/12 px-6 py-1 m1-1 me-4 mb-2">
-                        <MainP class="sr-only">Imagen</MainP>
-                        <MainImg class="w-full" :src="post.photoURL" />
+            <template v-if="!post.photoURL">
+                <PostEditSkeleton />
+            </template>
+            <template v-else>              
+                <form 
+                    action="#"
+                    @submit.prevent="submitPost">
+                    <div class="">
+                        <MainLabel for="usuario">Usuario :</MainLabel>
+                        <input
+                            class="px-4 py-2 text-xl font-bold rounded-lg"
+                            type="text" 
+                            id="usuario"
+                            disabled
+                            v-model="authUser.displayName">
                     </div>
-                </template>
-                <div class="mx-4">
-                    <MainLabel for="post">Publicacion</MainLabel>
-                    <textarea 
-                        class="w-full p-2 border-gray-200 border-2 rounded-lg focus:border-gray-200 focus:outline-none"
-                        name="post" 
-                        id="post"
-                        placeholder="Opina sobre que estas jugando"
-                        :disabled="postinSkeleton"
-                        v-model="post.post"></textarea>
-                </div>
-                <div class="mx-4">
-                    <MainP class="text-red-500 font-bold">{{feedbackMsg}}</MainP>
-                </div>
-                <div class="flex justify-between items-end">
-                    <div class="my-1 px-4">
-                        <MainLabel class="w-full not-sr-only inline px-4 py-2" for="photoURL">Agregar una imagen (opcional)</MainLabel>
-                        <input 
-                            type="file" 
-                            class="w-full p-2 bg-white border-gray-200 border-2 rounded-lg focus:border-gray-200 focus:outline-none" 
-                            id="photoURL" 
-                            name="photoURL"
-                            @change="selectPhoto">
+                    <template v-if="post.photoURL">
+                        <div class="w-6/12 px-6 py-1 m1-1 me-4 mb-2">
+                            <MainP class="sr-only">Imagen</MainP>
+                            <MainImg class="w-full" :src="post.photoURL" />
+                        </div>
+                    </template>
+                    <div class="mx-4">
+                        <MainLabel for="post">Publicacion</MainLabel>
+                        <textarea 
+                            class="w-full p-2 border-gray-200 border-2 rounded-lg focus:border-gray-200 focus:outline-none"
+                            name="post" 
+                            id="post"
+                            placeholder="Opina sobre que estas jugando"
+                            :disabled="postinSkeleton"
+                            v-model="post.post"></textarea>
                     </div>
-                    <div class="text-end mt-1">
-                        <button type="submit" 
-                            class=" px-6 py-1 m1-1 me-4 mb-2 rounded-lg text-xl text-end text-white bg-green-600 hover:bg-green-500 active:bg-green-700 transition-all">
-                            <template v-if="postinSkeleton">
-                                <PostingLoader />
-                            </template>
-                            <template v-else>
-                                Editar
-                            </template>
-                        </button>
+                    <div class="mx-4">
+                        <MainP class="text-red-500 font-bold">{{feedbackMsg}}</MainP>
                     </div>
-                </div>
-            </form>
+                    <div class="flex justify-between items-end">
+                        <div class="my-1 px-4">
+                            <MainLabel class="w-full not-sr-only inline px-4 py-2" for="photoURL">Agregar una imagen (opcional)</MainLabel>
+                            <input 
+                                type="file" 
+                                class="w-full p-2 bg-white border-gray-200 border-2 rounded-lg focus:border-gray-200 focus:outline-none" 
+                                id="photoURL" 
+                                name="photoURL"
+                                @change="selectPhoto">
+                        </div>
+                        <div class="text-end mt-1">
+                            <button type="submit" 
+                                class=" px-6 py-1 m1-1 me-4 mb-2 rounded-lg text-xl text-end text-white bg-green-600 hover:bg-green-500 active:bg-green-700 transition-all">
+                                <template v-if="postinSkeleton">
+                                    <PostingLoader />
+                                </template>
+                                <template v-else>
+                                    Editar
+                                </template>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </template>
         </section>
     </section>
 </template>
